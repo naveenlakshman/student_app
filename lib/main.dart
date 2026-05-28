d780d9fd757f0c56a332bb365be80082731e3b7a
Naveen Lakshman <hello.naveen9164@gmail.com>
2026-05-28 17:06:41 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: const LMSWebView(),
+    );
+  }
+}
+
+class LMSWebView extends StatefulWidget {
+  const LMSWebView({super.key});
+
+  @override
+  State<LMSWebView> createState() => _LMSWebViewState();
+}
+
+class _LMSWebViewState extends State<LMSWebView> {
+  late final WebViewController controller;
+
+  @override
+  void initState() {
+    super.initState();
+
+    controller = WebViewController()
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..loadRequest(
+        Uri.parse('https://www.globaliterp.com/student/login'),
+      );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: WebViewWidget(controller: controller),
+      ),
+    );
+  }
+}
